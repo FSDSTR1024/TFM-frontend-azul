eace293555a167a07b49e3536b391f90e790f5d1
ZilleriTKC <adrianguijarro42000@gmail.com>
2025-01-20 20:59:37 +0100

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import MainPage from './components/MainPage.jsx'
+
+function App() {
+
+  return (
+    <div className="App">
+      
+      <MainPage />
+    </div>
+  )
+}
+
+export default App;
